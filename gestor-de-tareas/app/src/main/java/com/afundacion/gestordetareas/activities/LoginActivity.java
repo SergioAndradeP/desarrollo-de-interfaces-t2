9a4f5d0888c1d0f5172d5e8a126577e7706b3893
Sergio Andrade <sandradep@fpcoruna.afundacion.org>
2023-01-11 10:30:25 +0100

    Añadidos los EditText y Botones al xml del LoginActivity

--- gestor-de-tareas/app/src/main/java/com/afundacion/gestordetareas/activities/LoginActivity.java
+++ gestor-de-tareas/app/src/main/java/com/afundacion/gestordetareas/activities/LoginActivity.java
@@ -1,6 +1,16 @@
 package com.afundacion.gestordetareas.activities;
 
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.afundacion.gestordetareas.R;
+
 public class LoginActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
 }
--- gestor-de-tareas/app/src/main/java/com/afundacion/gestordetareas/launcher/LauncherActivity.java
+++ gestor-de-tareas/app/src/main/java/com/afundacion/gestordetareas/launcher/LauncherActivity.java
@@ -7,13 +7,14 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.afundacion.gestordetareas.MainActivity;
+import com.afundacion.gestordetareas.activities.LoginActivity;
 
 public class LauncherActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         // Editad la actividad que lanza esta clase para ir probando lo que vais haciendo
-        Intent loginActivity = new Intent(this, MainActivity.class);
+        Intent loginActivity = new Intent(this, LoginActivity.class);
         startActivity(loginActivity);
     }
 }

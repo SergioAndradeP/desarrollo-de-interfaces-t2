d3f50e23cc15cb7b874d4dcaa154b6ea5db9f76a
Sergio Andrade <sandradep@fpcoruna.afundacion.org>
2023-01-16 17:28:01 +0100

    Añadido código de la clase Menu

--- gestor-de-tareas/app/src/main/java/com/afundacion/gestordetareas/launcher/LauncherActivity.java
+++ gestor-de-tareas/app/src/main/java/com/afundacion/gestordetareas/launcher/LauncherActivity.java
@@ -8,13 +8,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.afundacion.gestordetareas.MainActivity;
 import com.afundacion.gestordetareas.activities.LoginActivity;
+import com.afundacion.gestordetareas.utils.MenuActivity;
 
 public class LauncherActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         // Editad la actividad que lanza esta clase para ir probando lo que vais haciendo
-        Intent loginActivity = new Intent(this, LoginActivity.class);
+        Intent loginActivity = new Intent(this, MenuActivity.class);
         startActivity(loginActivity);
     }
 }

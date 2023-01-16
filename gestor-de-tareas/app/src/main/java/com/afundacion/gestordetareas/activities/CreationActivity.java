df102db4d5d4239ca130551d8367122ec4ed26c8
Christian <cmartinezguzman@fpcoruna.afundacion.org>
2023-01-16 17:03:41 +0100

    XML de creacion de tareas hecho, spinner añadido a java

--- gestor-de-tareas/app/src/main/java/com/afundacion/gestordetareas/activities/CreationActivity.java
+++ gestor-de-tareas/app/src/main/java/com/afundacion/gestordetareas/activities/CreationActivity.java
@@ -1,4 +1,30 @@
 package com.afundacion.gestordetareas.activities;
 
-public class CreationActivity {
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.afundacion.gestordetareas.R;
+
+public class CreationActivity extends AppCompatActivity {
+    Spinner spinner;
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_creation);
+
+        spinner = (Spinner) findViewById(R.id.tareas_spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.tareas_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+
+
+
+    }
 }

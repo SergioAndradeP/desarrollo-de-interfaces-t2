ff2b57c0b907c7e60614dcb26545f9f3310b7655
Sergio Andrade <sandradep@fpcoruna.afundacion.org>
2023-01-17 08:22:27 +0100

    Añadida clase RestClient a util la cual contendrá todas las peticiones de red

---
+++ gestor-de-tareas/app/src/main/java/com/afundacion/gestordetareas/utils/RestClient.java
@@ -0,0 +1,43 @@
+package com.afundacion.gestordetareas.utils;
+
+import static android.content.Context.MODE_PRIVATE;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.widget.EditText;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class RestClient {
+
+    private String BASE_URL = "";
+
+    private Context context;
+
+    private static RestClient singleton = null;
+
+    private RequestQueue queue;
+
+
+    private RestClient(Context context){
+        this.context = context;
+    }
+
+    public static RestClient getInstance(Context context){
+        if(singleton==null){
+            singleton = new RestClient(context);
+        }
+        return singleton;
+    }
+
+    // Métodos que lanzan peticiones
+}

6a9837340fe3b85fcc7fb7c774829aa622425644
HectorVara <hvaram@fpcoruna.afundacion.org>
2023-01-18 09:18:15 +0100

    Se pasa a mainfragment la mainactivity

--- gestor-de-tareas/app/src/main/java/com/afundacion/gestordetareas/MainActivity.java
+++ gestor-de-tareas/app/src/main/java/com/afundacion/gestordetareas/MainActivity.java
@@ -116,19 +116,6 @@ public class MainActivity extends AppCompatActivity {
 
         return inflatedView;
     }
-    private boolean DateIsFuture(String date){
-        Calendar c1 = Calendar.getInstance();
-        int pYear = c1.get(Calendar.YEAR);
-        int pMonth = c1.get(Calendar.MONTH)+1;
-        int pDay= c1.get(Calendar.DAY_OF_MONTH);
-        String[] dateSplitted= date.split("/",3);
-        if(Integer.parseInt(dateSplitted[2]) > pYear){
-            return true;
-        } else if (Integer.parseInt(dateSplitted[2])==pYear && Integer.parseInt(dateSplitted[1])>pMonth){
-            return true;
-        } else if(Integer.parseInt(dateSplitted[1])==pMonth && Integer.parseInt(dateSplitted[0])>=pDay){
-            return true;
-        }
-        return false;
-    }
+
+
 }
---
+++ gestor-de-tareas/app/src/main/java/com/afundacion/gestordetareas/Utils.java
@@ -0,0 +1,22 @@
+package com.afundacion.gestordetareas;
+
+import java.util.Calendar;
+
+public  class Utils {
+
+    public static boolean DateIsFuture(String date){
+        Calendar c1 = Calendar.getInstance();
+        int pYear = c1.get(Calendar.YEAR);
+        int pMonth = c1.get(Calendar.MONTH)+1;
+        int pDay= c1.get(Calendar.DAY_OF_MONTH);
+        String[] dateSplitted= date.split("/",3);
+        if(Integer.parseInt(dateSplitted[2]) > pYear){
+            return true;
+        } else if (Integer.parseInt(dateSplitted[2])==pYear && Integer.parseInt(dateSplitted[1])>pMonth){
+            return true;
+        } else if(Integer.parseInt(dateSplitted[1])==pMonth && Integer.parseInt(dateSplitted[0])>=pDay){
+            return true;
+        }
+        return false;
+    }
+}
